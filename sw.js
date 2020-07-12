f5280603875fb5465aded2718987fd178f7f993c
virn90 <40902887+virn90@users.noreply.github.com>
2020-07-13 05:27:27 +0700

    Update and rename sw.txt to sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 (function() {
   if ('serviceWorker' in navigator) {
     document.write('<script src="https://google.github.io/traceur-compiler/bin/traceur.js"></script>');
-    navigator.serviceWorker.register('swes6-worker.js', {scope: ''}).then(function() {
+    navigator.serviceWorker.register('https://cdn.statically.io/gh/virn90/seotechmancom/c542d5a6/service-worker.js', {scope: ''}).then(function() {
       console.info('SWES6 - Service worker registration succeeded.');
     }).catch(function(err) {
       console.warn('SWES6 - Service worker registration failed.');
