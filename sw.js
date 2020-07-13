814bb65cfef9368398f11143edd238a8a9bc0de5
virn90 <40902887+virn90@users.noreply.github.com>
2020-07-14 03:32:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,23 @@
-(function() {
-  if ('serviceWorker' in navigator) {
-    document.write('<script src="https://google.github.io/traceur-compiler/bin/traceur.js"></script>');
-    navigator.serviceWorker.register('https://cdn.statically.io/gh/virn90/seotechmancom/c542d5a6/service-worker.js', {scope: ''}).then(function() {
-      console.info('SWES6 - Service worker registration succeeded.');
-    }).catch(function(err) {
-      console.warn('SWES6 - Service worker registration failed.');
-      console.error(err);
-    });
-  } else {
-    console.warn('SWES6 - Service worker not availible.');
-  }
-}());
+---
+---
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open('blog').then(function(cache) {
+    return cache.addAll([
+      {% for page in site.pages %}
+      '{{ page.url | remove: '.html' }}',
+      {% endfor %}
+      {% for post in site.posts %}
+      '{{ post.url | remove: '.html' }}',
+      {% endfor %}
+      {% for file in site.static_files %}
+      '{{ site.baseurl }}{{ file.path }}',
+      {% endfor %}      
+    ]);
+  }));
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response) { 
+    return response;
+  }));
+});
