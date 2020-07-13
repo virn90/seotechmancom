c69bd9b2358e4bb8091f8eff51061e0aa0dd99be
virn90 <40902887+virn90@users.noreply.github.com>
2020-07-14 03:39:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
 ---
 ---
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open('blog').then(function(cache) {
+  e.waitUntil(caches.open('blog-{{ site.github.build_revision }}').then(function(cache) {
     return cache.addAll([
       {% for page in site.pages %}
       '{{ page.url | remove: '.html' }}',
       {% endfor %}
       {% for post in site.posts %}
       '{{ post.url | remove: '.html' }}',
-      {% endfor %}
-      {% for file in site.static_files %}
-      '{{ site.baseurl }}{{ file.path }}',
-      {% endfor %}      
+      {% endfor %}  
     ]);
   }));
 });
