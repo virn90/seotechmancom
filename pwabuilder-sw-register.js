fe8687fe7a4f5c2bed23d8488a48dc9bedfe1957
virn90 <40902887+virn90@users.noreply.github.com>
2020-07-14 16:56:15 +0700

    Update pwabuilder-sw-register.js

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -12,7 +12,7 @@
 if (somethingIsTrue) {
 await import ('https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate');
 }
-})();
+});
 
 const el = document.createElement('pwa-update');
 document.body.appendChild(el);
